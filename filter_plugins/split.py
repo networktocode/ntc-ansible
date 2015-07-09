3354d8c41bee7f78bf08433d4a2d1181fda04e64
root <root@unetlab.mk.net>
2015-07-09 21:06:55 +0000

    collapsed all roles in a single playbook

--- filter_plugins/split.py
+++ filter_plugins/split.py
@@ -1,22 +1,16 @@
 from ansible import errors
 import re
 
-def split_string(string, seperator=' '):
+def split_string(string, separator=' '):
     try:
-        return string.split(seperator)
+        return string.split(separator)
     except Exception, e:
         raise errors.AnsibleFilterError('split plugin error: %s, string=%s' % str(e),str(string) )
 
-def split_regex(string, seperator_pattern):
-    try:
-        return re.split(seperator_pattern, string)
-    except Exception, e:
-        raise errors.AnsibleFilterError('split plugin error: %s' % str(e))
 
 class FilterModule(object):
     ''' A filter to split a string into a list. '''
     def filters(self):
         return {
-            'split' : split_string,
-            'split_regex' : split_regex,
+            'split' : split_string
         }
