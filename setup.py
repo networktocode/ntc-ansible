54bf31506d83dda974be0ca60b90267cc18bc8b4
jmcgill298 <jacob@networktocode.com>
2019-10-09 14:09:56 -0400

    Update version and long_description

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
+import pathlib
 from setuptools import setup, find_packages
+
+
+CWD = pathlib.Path(__file__).parent
+README = (CWD / "README.md").read_text()
 
 setup(
   name='ntc-ansible',
   packages=find_packages(),
-  version='0.1.0',
+  version='0.9.1',
   description='Dependencies for NTC Ansible modules',
+  long_description=README,
+  long_description_content_type="text/markdown",
   author='Jason Edelman',
   author_email='jedelman8@gmail.com',
   url='https://github.com/networktocode/ntc-ansible',
