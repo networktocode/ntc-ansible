69f2772fd62868aa2bd5349e6fdb2a8eed6682d1
jmcgill298 <jacob@networktocode.com>
2019-10-22 11:09:30 -0400

    BugFix: Remove dependency on pathlib for python2.7

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+## [0.9.2]
+### Fixed
+- [#256] Made setup.py work with python2.7 again
+
--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
-import pathlib
+import os
 from setuptools import setup, find_packages
 
 
-CWD = pathlib.Path(__file__).parent
-README = (CWD / "README.md").read_text()
+CWD = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(CWD, "README.md")) as readme:
+    README = readme.read()
 
 setup(
   name='ntc-ansible',
   packages=find_packages(),
-  version='0.9.1',
+  version='0.9.2',
   description='Dependencies for NTC Ansible modules',
   long_description=README,
   long_description_content_type="text/markdown",
