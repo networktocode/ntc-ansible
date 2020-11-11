00ce962efb5f65d2489c9adda427b24a955c63df
Bart Smeding <mail@bartsmeding.nl>
2020-11-11 13:43:46 +0100

    wrong place of )

--- library/ntc_config_command.py
+++ library/ntc_config_command.py
@@ -147,7 +147,7 @@ try:
     HAS_PACKAGING=True
 except ImportError:
     HAS_PACKAGING=False
-if (HAS_PACKAGING and version.parse(ansible_version) < version.parse("2.4") or (not HAS_PACKAGING and float(ansible_version[:3]) < 2.4)):
+if (HAS_PACKAGING and version.parse(ansible_version) < version.parse("2.4")) or (not HAS_PACKAGING and float(ansible_version[:3]) < 2.4):
     raise ImportError("Ansible versions < 2.4 are not supported")
 
 
